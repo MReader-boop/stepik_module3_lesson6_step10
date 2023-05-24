e9ebad192c11f8922f2d5c70d2d87fe6dad64f15
klesov_pe <tester@gitea.local>
2023-05-24 13:09:06 +0300

    Correct test method name

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-def test_items(browser):
+def test_find_basket_button(browser):
     print('Browser initialised!')
     browser.implicitly_wait(10)
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
