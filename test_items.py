8c0e29e186093e988366390ca44aee2c044bfdf2
klesov_pe <tester@gitea.local>
2023-05-24 11:38:30 +0300

    Add test script

---
+++ test_items.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+def test_items(browser):
+    print('Browser initialised!')
+    browser.implicitly_wait(10)
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    add_to_basket_button = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket') #find_elements находит элементы и кладёт их в массив. Если массив пустой, значит мы не нашли искомую кнопку.
+    assert add_to_basket_button, '"Add to basket" button not found!'
