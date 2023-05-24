28e431fdfe9a238ae8b5aca6e41291b62a87c568
klesov_pe <tester@gitea.local>
2023-05-24 11:29:02 +0300

    Change Firefox options setup method

--- conftest.py
+++ conftest.py
@@ -1,20 +1,22 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='Chrome', help='Choose the browser for tests from the list in the following format: \nChrome \nFirefox\n-----\nDefault browser is Chrome.')
     parser.addoption('--language', action='store', default='en', help='Choose a language from the list in the following format: \nen\nru\nes\nfr\n-----\nDefault language is English (en).\n')
 
+#Настройку браузера производим в отдельной функции, чтобы не засорять фикстуру
 def browser_setup(browser_name, user_language):
     if browser_name == 'Chrome':
-        user_options = Options()
+        user_options = ChromeOptions()
         user_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         return webdriver.Chrome(options=user_options)
     elif browser_name == 'Firefox':
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        return webdriver.Firefox(firefox_profile=fp)
+        user_options = FirefoxOptions()
+        user_options.set_preference("intl.accept_languages", user_language)
+        return webdriver.Firefox(options=user_options)
     else:
         raise pytest.UsageError('--browser should be either Chrome of Firefox')
 
